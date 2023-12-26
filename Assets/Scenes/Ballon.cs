777e66ee2435ee8bda295c459b0cac47ad1f8734
Ameni14101998 <ameni.benrbiha14@gmail.com>
2023-12-26 18:14:12 +0100

    Premier commit avec mon projet

---
+++ Assets/Scenes/Ballon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+public class Ballon : MonoBehaviour
+{
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //retourne type 
+        rb.isKinematic = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /*
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z + transform.position.z);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); //prend position de la souris
+        transform.position = objPosition;
+        rb.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        rb.isKinematic = false;
+    }
+    */
+     void OnMouseDown()
+    {
+        if (Boutton.ballbool == 1)
+        {
+            rb.isKinematic = false;
+            Boutton.ballbool = 0;
+        }
+    }
+ }
+
---
+++ Assets/Scenes/Boutton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boutton : MonoBehaviour
+{
+    public string scene;
+    public string next;
+    public static int ballbool = 1;
+    public static int a = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void replay()
+    {
+        SceneManager.LoadScene(scene);
+        ballbool = 1;
+        a = 1;
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene("menu");
+        ballbool = 1;
+        a = 1;
+    }
+    public void nexts()
+    {
+        SceneManager.LoadScene(next);
+        ballbool = 1;
+        a = 1;
+    }
+    public void exit()
+    {
+        ballbool = 1;
+        a = 1;
+        Application.Quit();
+
+        
+    }
+}
